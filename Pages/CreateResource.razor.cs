f14760c0473979dd152d91fce3f9b40bbe26030a
gujo <gujo@itu.dk>
2021-12-10 17:47:24 +0100

    added initial ui for createResource

---
+++ Pages/CreateResource.razor.cs
@@ -0,0 +1,20 @@
+
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Components;
+// using Models;
+// using Services;
+
+
+// public partial class CreateResourse
+// {
+//     public ResourceRequestModel _request = new ResourceRequestModel();
+//     public ResourceResponseModel _response = new ResourceResponseModel();
+
+//     [Inject]
+//     private IResourceService _service { get; set; }
+
+//     public async Task HandleValidSubmit()
+//     {
+//         _response = await _service.CreateResource(_request);
+//     }
+// }
