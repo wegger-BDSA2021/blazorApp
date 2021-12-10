306c77734f5678e9507692f8c676a856b766de7b
gujo <gujo@itu.dk>
2021-12-10 17:08:44 +0100

    added models to match api

--- Models/ResourceRequestModel.cs
+++ Models/ResourceRequestModel.cs
@@ -2,6 +2,10 @@ namespace Models
 {
     public class ResourceRequestModel
     {
-        
+        public string Title { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public int InitialRating { get; set; }
     }
 }
--- Models/ResourceResponseModel.cs
+++ Models/ResourceResponseModel.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+
 namespace Models
 {
     public class ResourceResponseModel
     {
-        
+        public string TitleFromUser { get; set; }
+        public string TitleFromSource { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeOfReference { get; set; }
+        public string Url { get; set; }
+        public string HostBaseUrl { get; set; }
+        public ICollection<string> TagsFoundInSource { get; set; } = new List<string>();
+        public int InitialRating { get; set; }
+        public Boolean Deprecated { get; set; }
+        public DateTime LastCheckedForDeprecation { get; set; }
+        public bool IsVideo { get; set; }
+        public bool IsOfficialDocumentation { get; set; }
     }
 }
